1b0252df46de5881e746058e6075311e749e985d
hnc-roblox <banhbaothanthanh4@gmail.com>
2025-08-25 20:07:33 +0700

    Update HNC_Roblox.lua

--- HNC_Roblox.lua
+++ HNC_Roblox.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hnc-roblox/O/refs/heads/main/%C4%90%E1%BA%A7u%20Bu%E1%BB%93i%3A).lua"))()
+loadstringHttpGet("https://raw.githubusercontent.com/hnc-roblox/O/refs/heads/main/%C4%90%E1%BA%A7u%20Bu%E1%BB%93i%3A).lua"))()
